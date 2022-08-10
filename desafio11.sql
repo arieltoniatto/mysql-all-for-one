26bcb85a5e3fb3ed2ab966f7dfc8b53bc334e845
arieltoniatto <ariel.toniatto@gmail.com>
2022-08-10 17:34:56 -0300

    adiciona outras formas de resolver o requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,6 @@
 SELECT notes FROM northwind.purchase_orders
-WHERE notes REGEXP 'Purchase generated based on Order #3[0-9]$';
+WHERE notes LIKE '%3_';
 
+-- WHERE notes LIKE '%3%';
+-- WHERE notes REGEXP 'Purchase generated based on Order #3[0-9]$';
 -- https://dev.mysql.com/doc/refman/8.0/en/regexp.html#operator_regexp
